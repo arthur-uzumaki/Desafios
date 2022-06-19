9f3fcdedeea1a36b755add4d419ac55d28978aa2
Arttanjeiro <102182959+Arttanjeiro@users.noreply.github.com>
2022-06-19 11:42:27 -0300

    Organizando site para celular

--- desafio01/index.js
+++ desafio01/index.js
@@ -1,7 +1,9 @@
 const entrar = document.getElementById("entrar")
+const url = 'https://www.google.com/'
 
 entrar.addEventListener('click' , (evento)=> {
     evento.preventDefault()
+   
 
     const email = document.getElementById("email")
     const senha = document.getElementById("senha")
@@ -9,21 +11,33 @@ entrar.addEventListener('click' , (evento)=> {
     if(email.value == ""){
         email.classList.add('erroInput')
     }else{
+       
         email.classList.remove('erroInput')
+       
     }
     if(senha.value == ""){
         senha.classList.add("erroInput")
+        
     }else{
+        
         senha.classList.remove('erroInput')
+
     }
     if (email.value.indexOf("@") ==-1 || email.value.indexOf(".") == -1 || email.value.indexOf(".") - email.value.indexOf("@")==1){
         email.classList.add('erroInput')
     }else{
+        
         email.classList.remove('erroInput')
     }
     if (senha.value.length <= 5) {
         senha.classList.add("erroInput")
     }else{
+       
         senha.classList.remove('erroInput')
     }
+    function cadastro(url) {
+        const saite = window.open(url , '_blank')
+        saite.focus()
+    }
+ 
 })
