2324946a8a2dfdf8f06b24a0a52bb3b697a881f0
Arttanjeiro <102182959+Arttanjeiro@users.noreply.github.com>
2022-11-07 14:40:13 -0300

    Arrumando  projeto 14

--- desafio14/nlw/server/prisma/migrations/20221102020435_create_bd/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[userId,poolId]` on the table `Participant` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Participant_userId_poolId_key" ON "Participant"("userId", "poolId");
